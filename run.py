20e8b180d0a42466e4d1a641750fbba602ce2fc6
brodsa <brodinova@gmail.com>
2023-06-28 22:44:34 +0000

    feat: add welcoming message

--- run.py
+++ run.py
@@ -6,6 +6,31 @@
 from revisport import helpers
 from revisport import SHEET
 
+# data preparation
+# data = helpers.prepare_data(SHEET,'owid-co2-data','filter')
+# print(data.head())
 
-data = helpers.prepare_data(SHEET,'owid-co2-data','filter')
-print(data)
+
+def main_menu():
+    # welcome message
+    print("Welcome to ReVisPort!\n")
+    print("Your command line reporting tool is ready for you to use.\n")
+    print("Please reade carefully and follow the instructions.\n")
+    print("\n")
+
+    # welcome qustion
+    print("Select the item from the menu:")
+    print("1: Reporting")
+    print("2: Favourite")
+    print("3: References")
+
+    
+    answer = input()
+
+    print(f"You have selected {answer}")
+
+
+
+
+
+main_menu()
