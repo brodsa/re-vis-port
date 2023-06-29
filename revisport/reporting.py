27999c36ce032f1cb5d24e4638c35a5911100879
brodsa <brodinova@gmail.com>
2023-06-29 17:14:19 +0000

    feat: add ready for reporting

--- revisport/reporting.py
+++ revisport/reporting.py
@@ -1,28 +1,36 @@
+import revisport as rvp 
+from revisport import SHEET
+
 def reporting_menu():
     print("\nYou are going to create a simple report.")
-    print("The report contains a table with the information about countries and climate indices.")
-    print("The programm navigates you to create such report.\n")
+    print("The report contains a table with the information about EU countries and climate indices.")
+    print("The programm navigates you to generate the report table.\n")
 
     while True:
         try:
-            answer = input("Are you ready (y/n)?")[0].lower().strip()
-            
-            if answer=='y':
-                print('you are about to create a report')
-                break
-            
-            elif answer=='n':
-                print('Oh, would')
+            answer = input(
+                "Are you ready (y - yes, continue / n - no, go back)?"
+                )[0].lower().strip()
+            print(answer)
+
+            if answer in ['y','n']:
+                if answer=='n':
+                    rvp.main_menu.welcome_menu()
+                    rvp.main_menu.get_answer()
+                else:
+                    reporting_questions()
                 break
 
             else:
                 raise ValueError(f"Only y/n is allowed, you wrote {answer}")
 
 
+        except (ValueError,IndexError) as e:
+                print(f"Invalid input: {e}; please try again. \n")
+        
 
-
-        except ValueError as e:
-                print(f"Invalid data: {e}, please try again. \n")
-
+    
+def reporting_questions():
+    print("Select ISO code of an EU country for which you wish to create a report!")
     
         
